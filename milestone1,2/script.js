09cd1ddadb7f3c79c3b32283933833c7af26cbd3
Areebakhan8 <160344226+Areebakhan8@users.noreply.github.com>
2024-11-11 01:07:53 +0500

    Add files via upload

---
+++ milestone1,2/script.js
@@ -0,0 +1,10 @@
+var toggle = document.getElementById('button');
+var skill = document.getElementById('skills');
+toggle.addEventListener('click', function () {
+    if (skill.style.display === 'none') {
+        skill.style.display = 'block';
+    }
+    else {
+        skill.style.display = 'none';
+    }
+});
